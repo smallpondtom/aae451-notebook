5a4e1f5b29cecf9ab65f9001dca5ef0eb6b65b31
smallpondtom <usasmpdtom@gmail.com>
2021-04-23 15:27:09 -0400

    push the changes.

--- MATLAB-controls/hover/roll.m
+++ MATLAB-controls/hover/roll.m
@@ -5,11 +5,20 @@ clear all; close all; clc;
 num = 76.15;  % numerator 
 den = [1, 0, 0];  % denominator
 
-Kp = 7.66302615547072e-10;
-Kd = 0.0076620813293718;
-N = 505.040986830046;
+% Kp = 7.66302615547072e-10;
+% Kd = 0.0076620813293718;
+% N = 505.040986830046;
+
+Kp = 0.0205;
+Kd = 0.0215;
+N = 10;
 
 c1 = N*Kd + Kp;
 c2 = N*Kp;
 c3 = 1;
 c4 = N;
+
+s = tf('s');
+G = tf(num, den);
+H = (c1*s + c2) / (c3*s + c4);
+bw = bandwidth(feedback(G*H, 1));
